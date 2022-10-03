430d40596ec23383774da9ada3bf90c04413a11d
Juliette Franqueville <juliette.franqueville@gmail.com>
2022-10-03 10:47:58 -0500

    hw2

---
+++ homework_2/python_code/optimizations.py
@@ -0,0 +1,230 @@
+""" . """
+import numpy as np
+import warnings
+
+
+class GradientAscent:
+    """
+    do gradient ascent :-P
+    """
+
+    def __init__(self, y_obs, lr=.01, max_iter=5000, tol=1e-5):
+        """
+
+        Parameters
+        ----------
+        y_obs: array like
+            observed data
+        lr: float
+            learning rate
+        max_iter: int
+            max number of iterations
+        tol: float
+            tolerance
+        """
+        self.lr = lr
+        self.max_iter = max_iter
+        self.init_points = np.linspace(y_obs.min(), y_obs.max(), 10)
+        self.tol = tol
+        self.optimal_theta = None
+        self.it_to_convergence = None
+        self.y_obs = y_obs
+
+    def initialize_params(self, init_theta, y_obs):
+        """
+
+        Parameters
+        ----------
+        y_obs
+        init_theta: float
+            initial theta guess
+
+        Returns
+        -------
+
+        """
+        thetas = np.zeros(self.max_iter)
+        thetas[0] = init_theta
+        log_likelihoods = np.zeros(self.max_iter)
+        log_likelihoods[0] = self.calculate_log_likelihood(thetas[0], y_obs)
+        return thetas, log_likelihoods
+
+    @staticmethod
+    def calculate_log_likelihood(theta, y_obs):
+        """
+
+        Parameters
+        ----------
+        y_obs
+        theta: float
+            theta to evaluate log like at
+
+        Returns
+        -------
+
+        """
+        return np.sum(np.log(1 / np.pi * 1 / (1 + (y_obs - theta) ** 2)))
+
+    @staticmethod
+    def calculate_derivative_log_likelihood(theta, y_obs):
+        """
+
+        Parameters
+        ----------
+        y_obs
+        theta: float
+            theta to evaluate d/d theta log like at
+
+        Returns
+        -------
+
+        """
+        return np.sum(2 * (y_obs - theta) / (1 + (theta - y_obs) ** 2))
+
+    def check_convergence(self, it, log_likelihoods):
+        """
+
+        Parameters
+        ----------
+        it: int
+            iteration number
+        log_likelihoods: array like
+            array of thetas
+
+
+        Returns
+        -------
+
+        """
+        return np.abs((log_likelihoods[it + 1] - log_likelihoods[it]) / log_likelihoods[it]) < self.tol
+
+    def fit_one_initial_point(self, init_theta):
+        """
+
+        Parameters
+        ----------
+        init_theta: float
+            initial theta guess
+
+        Returns
+        -------
+
+        """
+        thetas, log_likelihoods = self.initialize_params(init_theta, self.y_obs)
+        for it in range(self.max_iter - 1):
+            thetas[it + 1] = thetas[it] + self.lr * self.calculate_derivative_log_likelihood(thetas[it], self.y_obs)
+            log_likelihoods[it + 1] = self.calculate_log_likelihood(thetas[it + 1], self.y_obs)
+            if self.check_convergence(it, log_likelihoods):
+                return thetas[it + 1], log_likelihoods[it + 1], it
+        warnings.warn(f'not converged for init point {init_theta}')
+        return thetas[-1], log_likelihoods[-1], self.max_iter
+
+    def fit(self):
+        """
+
+        Returns
+        -------
+        MLE for theta
+
+        """
+        optimal_thetas = np.zeros(len(self.init_points))
+        maximum_likelihoods = np.zeros(len(self.init_points))
+        n_iter = np.zeros(len(self.init_points))
+        for n, point in enumerate(self.init_points):
+            optimal_thetas[n], maximum_likelihoods[n], n_iter[n] = self.fit_one_initial_point(point)
+        self.optimal_theta = optimal_thetas[np.argmax(maximum_likelihoods)]
+        self.it_to_convergence = n_iter[np.argmax(maximum_likelihoods)]
+        return {'optimal theta': self.optimal_theta, 'n_iter': self.it_to_convergence}
+
+
+class StochasticGradientAscent(GradientAscent):
+    """
+        inherits gradient ascent and does sga
+    """
+
+    def __init__(self, y_obs, lr=.01, max_iter=1000, tol=1e-5, percent_batch_size=.3):
+        super().__init__(y_obs=y_obs, lr=lr, max_iter=max_iter, tol=tol)
+        self.percent_batch_size = percent_batch_size
+
+    def fit_one_initial_point(self, init_theta):
+        """
+
+        Parameters
+        ----------
+        init_theta: float
+            initial theta guess
+
+        Returns
+        -------
+
+        """
+        thetas, log_likelihoods = self.initialize_params(init_theta, self.y_obs)
+        for it in range(self.max_iter - 1):
+            selected_obs = np.random.choice(a=self.y_obs, size=int(len(self.y_obs) * self.percent_batch_size),
+                                            replace=False)
+            thetas[it + 1] = thetas[it] + self.lr * self.calculate_derivative_log_likelihood(thetas[it], selected_obs)
+            log_likelihoods[it + 1] = self.calculate_log_likelihood(thetas[it + 1], self.y_obs)
+            if self.check_convergence(it, log_likelihoods):
+                return thetas[it + 1], log_likelihoods[it + 1], it
+        warnings.warn(f'not converged for init point {init_theta}')
+        return thetas[-1], log_likelihoods[-1], self.max_iter
+
+
+class NewtonMethod(GradientAscent):
+    """
+    Newton's method
+    """
+    def fit_one_initial_point(self, init_theta):
+        """
+
+        Parameters
+        ----------
+        init_theta: float
+            initial theta guess
+
+        Returns
+        -------
+
+        """
+        thetas, log_likelihoods = self.initialize_params(init_theta, self.y_obs)
+        for it in range(self.max_iter - 1):
+            thetas[it + 1] = thetas[it] - 1 / self.calculate_hessian(thetas[it], self.y_obs) \
+                             * self.calculate_derivative_log_likelihood(thetas[it], self.y_obs)
+            log_likelihoods[it + 1] = self.calculate_log_likelihood(thetas[it + 1], self.y_obs)
+            if self.check_convergence(it, log_likelihoods):
+                return thetas[it + 1], log_likelihoods[it + 1], it
+        warnings.warn(f'not converged for init point {init_theta}')
+        return thetas[-1], log_likelihoods[-1], self.max_iter
+
+    @staticmethod
+    def calculate_hessian(theta, y_obs):
+        """
+
+        Parameters
+        ----------
+        theta
+        y_obs
+
+        Returns
+        -------
+
+        """
+        jacobian = np.sum(2 * ((theta - y_obs) ** 2 - 1) / (1 + (y_obs - theta) ** 2) ** 2)
+        return jacobian
+
+
+if __name__ == '__main__':
+    observed_data = np.array(
+        [7.52, 9.92, 9.52, 21.97, 8.39, 8.09, 9.22, 9.37, 7.33, 15.32, 1.08, 8.51, 17.73, 11.20, 8.33, 10.83, 12.40,
+         14.49, 9.44, 3.67])
+
+    ga = GradientAscent(y_obs=observed_data)
+    ga.fit()
+    sga = StochasticGradientAscent(y_obs=observed_data)
+    sga.fit()
+    nt = NewtonMethod(y_obs=observed_data)
+    nt.fit()
+    print('Gradient Ascent iter :', ga.it_to_convergence)
+    print('Stochastic gradient Ascent iter :', sga.it_to_convergence)
+    print('Newton Ascent iter :', nt.it_to_convergence)
+    pass
---
+++ homework_2/python_code/weibull_mle.py
@@ -0,0 +1,139 @@
+""" . """
+import numpy as np
+import warnings
+
+
+class WeibullMle:
+    """
+    Fit k and lambda via MLE
+    """
+
+    def __init__(self, data, max_iter=1000, lr=.001, tol=1e-5, init_params=None):
+        """
+
+        Parameters
+        ----------
+        data
+        max_iter
+        lr
+        tol
+        """
+        self.data = data
+        self.max_iter = max_iter
+        self.lr = lr
+        self.tol = tol
+        self.k = None
+        self.lam = None
+        self.n = len(data)
+        self.init_params = init_params
+
+    def initialize_opt(self):
+        """
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+
+        """
+        if self.init_params is None:
+            self.init_params = [200, 10]
+        params = np.zeros((2, self.max_iter))
+        log_likelihoods = np.zeros(self.max_iter)
+        params[:, 0] = self.init_params
+        log_likelihoods[0] = self.calculate_log_likelihood(*params[:, 0])
+        return params, log_likelihoods
+
+    def calculate_hessian(self, lam, k):
+        """
+
+        Parameters
+        ----------
+        lam
+        k
+
+        Returns
+        -------
+
+        """
+
+        d_k_lam = np.sum(((self.data / lam) ** k + k * (self.data / lam) ** k * np.log(self.data / lam) - 1) / lam)
+        d_k_k = np.sum((self.data/lam)**k * (-np.log(self.data/lam)**2)-1/k**2)
+        d_lam_lam = - np.sum((k*(k*(self.data/lam)**k+(self.data/lam)**k-1))/lam**2)
+        return np.array(([d_lam_lam, d_k_lam], [d_k_lam, d_k_k]))
+
+    def calculate_derivative_log_likelihood(self, lam, k):
+        """
+
+        Parameters
+        ----------
+        lam
+        k
+
+        Returns
+        -------
+
+        """
+        d_k = np.sum(1 / k - ((self.data / lam) ** k - 1) * np.log(self.data / lam))
+        d_lam = np.sum(k / lam * ((self.data / lam) ** k - 1))
+        return np.array([d_lam, d_k])
+
+    def calculate_log_likelihood(self, lam, k):
+        """
+
+        Parameters
+        ----------
+        lam
+        k
+
+        Returns
+        -------
+
+        """
+        log_likelihood = self.n * np.log(k) - self.n * np.log(lam) + (k - 1) * np.sum(np.log(self.data / lam)) - np.sum(
+            (self.data / lam) ** k)
+        return log_likelihood
+
+    def check_convergence(self, it, log_likelihoods):
+        """
+
+        Parameters
+        ----------
+        it
+        log_likelihoods
+
+        Returns
+        -------
+
+        """
+        return np.abs((log_likelihoods[it + 1] - log_likelihoods[it]) / log_likelihoods[it]) < self.tol
+
+    def fit(self):
+        """
+
+        Returns
+        -------
+
+        """
+
+        params, log_likelihoods = self.initialize_opt()
+        for it in range(self.max_iter - 1):
+            hessian = self.calculate_hessian(*params[:, it])
+            new_params = params[:, it] - np.linalg.inv(hessian) \
+                @ self.calculate_derivative_log_likelihood(*params[:, it])
+            params[:, it+1] = np.array([max(new_params[0], 1e-6), max(new_params[1], 1e-6)])
+            log_likelihoods[it + 1] = self.calculate_log_likelihood(*params[:, it + 1])
+            if self.check_convergence(it, log_likelihoods):
+                self.lam, self.k = params[:, it + 1]
+                return self.lam, self.k, log_likelihoods[it + 1]
+        warnings.warn(f'not converged')
+        self.lam, self.k = params[:, -1]
+        return self.lam, self.k, log_likelihoods[-1]
+
+
+if __name__ == '__main__':
+    obs_data = np.array([225, 171, 198, 189, 189, 135, 162, 135, 117, 162])
+    opt = WeibullMle(obs_data)
+    opt.fit()
+    pass
